49f1950096243c1c8648dda79e9a1ea169b3076f
akanshv <akanshv2003@gmail.com>
2023-04-24 23:34:06 +0530

    Filter in Experts

--- routes/therapyrouts.js
+++ routes/therapyrouts.js
@@ -25,6 +25,69 @@ router.get('/',catchAsync(async (req, res, next) => {
 
 }))
 
+router.get('/:no',catchAsync(async(req,res)=>{
+  navactive=[0,0,0,1,0,0]
+  type=req.params.no;
+  console.log(typeof(type));
+  doctor = await experts.find({});
+  let expertarray=[];
+  if(type ==='1'){
+    for (let doc of doctor) {
+      if(doc.ExpertsIn.includes("Relationship")){
+        expertarray.push(doc);
+      }
+    }
+    console.log(expertarray)
+  }
+  else if(type==='2'){
+    for (let doc of doctor) {
+      if(doc.ExpertsIn.includes("Work Stress")){
+        expertarray.push(doc);
+      }
+    }  }
+  else if(type==='3'){
+    for (let doc of doctor) {
+      if(doc.ExpertsIn.includes("Teen Problems")){
+        expertarray.push(doc);
+      }
+    }  }
+  else if(type==='4'){
+    for (let doc of doctor) {
+      if(doc.ExpertsIn.includes("Substance Abuse")){
+        expertarray.push(doc);
+      }
+    }  }
+  else if(type==='5'){
+    for (let doc of doctor) {
+      if(doc.ExpertsIn.includes("Sexual Abuse")){
+        expertarray.push(doc);
+      }
+    }  }
+  else if(type==='6'){
+    for (let doc of doctor) {
+      if(doc.ExpertsIn.includes("Harrassment")){
+        expertarray.push(doc);
+      }
+    }  }
+  else if(type==='7'){
+    for (let doc of doctor) {
+      if(doc.ExpertsIn.includes("Loneliness")){
+        expertarray.push(doc);
+      }
+    }  }
+  else if(type==='8'){
+    for (let doc of doctor) {
+      if(doc.ExpertsIn.includes("Anxiety")){
+        expertarray.push(doc);
+      }
+    }  }
+  else{
+      return res.redirect('/therapy');
+  }
+
+  res.render('therapy/therapyentry', {expertarray:expertarray,navactive:navactive});
+
+}))
 
 
 
